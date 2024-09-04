0f9ab4ba568a4dbce40057cd5542ce1152370775
Ed Vidal Jr <develone@sbcglobal.net>
2024-09-04 09:51:28 -0600

    initial steps

--- verilog-uart/nandland/top.v
+++ verilog-uart/nandland/top.v
@@ -9,8 +9,25 @@ module top (
 );
 reg out1;
 
-  always @(posedge CLK) begin
-    if (UART_RX == 1'b0) 
+parameter c_CLKS_PER_BIT    = 104;
+reg r_TX_DV = 0;
+reg [7:0] r_TX_Byte = 0;
+wire [7:0] w_RX_Byte;
+reg [7:0] r_RX_Byte;
+UART_RX #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) UART_RX_Inst
+    (.i_Clock(CLK),
+     .i_RX_Serial(UART_RX),
+     .o_RX_DV(w_RX_DV),
+     .o_RX_Byte(w_RX_Byte)
+    );
+
+UART_TX #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) UART_TX_Inst
+    (.i_Clock(CLK),
+     .i_TX_DV(r_TX_DV),
+     .i_TX_Byte(r_TX_Byte)
+    );
+  always @(posedge w_RX_DV) begin
+    if (w_RX_Byte == 8'h31) 
     begin
 	out1 <= 1'b0;
     end
@@ -20,4 +37,5 @@ reg out1;
     assign UART_TX = UART_RX;
     
     assign LED_G = out1;
+    assign r_TX_Byte = r_RX_Byte;
 endmodule
