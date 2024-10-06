cb6273ebe2a4931b9e235e40302c999d51fcc57f
Ed Vidal Jr <develone@sbcglobal.net>
2024-10-06 10:51:39 -0600

    adding psram

--- verilog-catzip-uart/top-40.v
+++ verilog-catzip-uart/top-40.v
@@ -14,6 +14,17 @@ output wire [2:0] led
 parameter [31:0] uarts=2;
 //LEDs
 
+  //access to spram
+  reg [15:0] ram_addr;
+  wire [15:0] ram_data_in;
+  wire [15:0] ram_data_out;
+  wire ram_wren;
+
+  reg [7:0] state;
+
+  reg [31:0] counter;
+
+parameter IDLE = 0, INIT0 = IDLE+1, INIT1=INIT0+1, INIT2=INIT1+1, INIT3=INIT2+1, RUN = INIT3+1 ;
 
 
 parameter BIT_WIDTH = 11;
@@ -74,13 +85,13 @@ assign	s_clk = clk_40mhz;
   always @(posedge s_clk) begin
     if(sig_valid[0] == 1'b1) begin
       if(sig_rx_data[7:0] == 8'h31) begin
-        sig_led[0] <=  ~sig_led[0];
+        //sig_led[0] <=  ~sig_led[0];
       end
       else if(sig_rx_data[7:0] == 8'h32) begin
-        sig_led[1] <=  ~sig_led[1];
+        //sig_led[1] <=  ~sig_led[1];
       end
       else if(sig_rx_data[7:0] == 8'h33) begin
-        sig_led[2] <=  ~sig_led[2];
+        //sig_led[2] <=  ~sig_led[2];
       end
       else if(sig_rx_data[7:0] == 8'h34) begin
         sig_led[3] <=  ~sig_led[3];
@@ -104,6 +115,84 @@ assign	s_clk = clk_40mhz;
       .tx_data(sig_tx_data[7 + 8 * i -: 7 + 1]));
 
   end
+
+
   endgenerate
+
+  SB_SPRAM256KA spram
+  (
+    .ADDRESS(ram_addr),
+    .DATAIN(ram_data_in),
+    .MASKWREN({ram_wren, ram_wren, ram_wren, ram_wren}),
+    .WREN(ram_wren),
+    .CHIPSELECT(1'b1),
+    .CLOCK(s_clk),
+    .STANDBY(1'b0),
+    .SLEEP(1'b0),
+    .POWEROFF(1'b1),
+    .DATAOUT(ram_data_out)
+  );
+
+  initial begin
+      state <= INIT0;
+      led <= 3'b000;
+      counter <= 0;
+  end
+
+  always @(posedge s_clk)
+  begin
+
+    ram_wren <= 1'b0;
+
+    case(state)
+    IDLE:
+    begin
+    end
+    INIT0:
+    begin
+      ram_addr <= 16'b00;
+      ram_data_in <= 16'b001; //write red
+      ram_wren <= 1'b1;
+      state <= INIT1;
+    end
+    INIT1:
+    begin
+      ram_addr <= 16'b01;
+      ram_data_in <= 16'b010; //write green
+      ram_wren <= 1'b1;
+      state <= INIT2;
+    end
+    INIT2:
+    begin
+      ram_addr <= 16'b10;
+      ram_data_in <= 16'b100; //write blue
+      ram_wren <= 1'b1;
+      state <= INIT3;
+    end
+    INIT3:
+    begin
+      ram_addr <= 16'b11;
+      ram_data_in <= 16'b111; //write white
+      ram_wren <= 1'b1;
+      state <= RUN;
+    end
+    RUN:
+    begin
+      counter <= counter + 1;
+
+      //incrment address every ~sec at 12Mhz
+      if(counter == 32'h1000000) begin
+        ram_addr[1:0] <= ram_addr[1:0] + 1;
+      end
+      if(counter == 32'h1000002) begin //must wait two cycles to have data
+         led <= ram_data_out[2:0];
+         counter <= 0;
+      end
+
+    end
+    endcase
+
+
+  end
 
 endmodule
