977085f5005c49ce37db3d87fedce72d31d15e4a
Edward Vidal Jr <develone@sbcglobal.net>
2025-03-06 12:23:25 -0700

    adding 3rd step of build process

--- PipelineC/pmod-ethernet/examples/pico/ice_makefile_pipelinec/build.sh
+++ PipelineC/pmod-ethernet/examples/pico/ice_makefile_pipelinec/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 make clean
 make pipelinec TOP_NAME=ethernet_top  NEXTPNR_ARGS="--pre-pack eth_clocks.py"
+make gateware.bin TOP_NAME=ethernet_top  NEXTPNR_ARGS="--pre-pack eth_clocks.py"
