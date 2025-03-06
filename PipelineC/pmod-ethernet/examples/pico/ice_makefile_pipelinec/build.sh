ba17ec9e7943d4ee1eb2e276b6b0bff2b4ef5642
Edward Vidal Jr <develone@sbcglobal.net>
2025-03-06 10:25:06 -0700

    fixing build.sh script

--- PipelineC/pmod-ethernet/examples/pico/ice_makefile_pipelinec/build.sh
+++ PipelineC/pmod-ethernet/examples/pico/ice_makefile_pipelinec/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 make clean
-make pipelinec TOP_NAME=ethernet_top  PLL_CLK_MHZ=25.0: NEXTPNR_ARGS="--pre-pack eth_clocks.py"
+make pipelinec TOP_NAME=ethernet_top  NEXTPNR_ARGS="--pre-pack eth_clocks.py"
