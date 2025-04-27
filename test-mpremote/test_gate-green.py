ea674939d12b620fa4e04ebc0cba7f9cbe750a76
Edward Vidal Jr <develone@sbcglobal.net>
2025-04-27 06:44:03 -0600

    testing micropython on pico-ice (env) devel@pi5-90:~/pico-ice/test-mpremote $ mpremote Connected to MicroPython at /dev/ttyACM0 Use Ctrl-] or Ctrl-x to exit this shell MicroPython v1.25.0 on 2025-04-24; tinyVision.ai pico-ice with RP2040 Type "help()" for more information. >>>

---
+++ test-mpremote/test_gate-green.py
@@ -0,0 +1,6 @@
+from machine import Pin
+import ice
+fpga = ice.fpga(cdone=Pin(26), clock=Pin(24), creset=Pin(27), cram_cs=Pin(9), cram_mosi=Pin(8), cram_sck=Pin(10), frequency=48)
+file = open("gateware-green.bin","br")
+fpga.start()
+fpga.cram(file)
